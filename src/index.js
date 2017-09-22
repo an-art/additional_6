ab2105c67c136563ce6ea79b34ab27f1fe8e0fd7
an-art <anartyomchik@gmail.com>
2017-09-22 16:58:16 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
-module.exports = function zeros(expression) {
-  var arr = expression.split('');
-  var a, fac;
-  
-  for (var i = arr.length-1; i >= 0; i--) {
-  	if (arr[i] === '!' && arr[i-1] === '!') {
-  		a = +arr.slice(0, i-1).join('');
-  		fac = factorial2(a);
-  		return count_zeros(fac);
-  	}
-  	else {
-  		a = +arr.slice(0, i).join('');
-  		fac = factorial(a);
-  		return count_zeros(fac);
-  	}
-  }
-  function factorial(n) {
-  	return n ? n * factorial(n - 1) : 1;
-  }
+module.exports = function zeros(str) {
+  var arr = [];
+  var count = 0;
+  var b;
 
-  function factorial2(n) {
-  	return (n!=1 && n) ? n * factorial2(n - 2) : 1;
-  }
-  function count_zeros(n) {
-  	var count = 0;
-  	while (n%10 === 0) {
-		n = n/10;
-		count++;
+  if (str.includes('*')) arr = str.split("*");
+  else arr = [str];
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i].endsWith('!!')) {
+      arr[i] = arr[i].slice(0, -2);
+      b = +arr[i];
+      while (b) {
+        if (b%5 === 0) count++;
+        b -= 2;
+      }
+    } else {
+      arr[i] = arr[i].slice(0, -1);
+      for (var j=5; arr[i]/j>=1; j*=5) {
+        count += Math.floor(arr[i]/j);
+      }
     }
-  	return count;
   }
+  return count;
 }
-//проверить есть ли *, если да, то разбить на массив по *
